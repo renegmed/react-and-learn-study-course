a7377179f0859bb1f3a127433a9178c0517ca1e6
Rene Medalla <renemed@pop-os.localdomain>
2022-08-23 10:36:55 -0400

    sec 04: 30. Typing an Action

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-//import EventComponent from './events/EventComponent';
-import UserSearch from './refs/UserSearch';
-
 function App() {
   return (
     <div>       
-      <UserSearch />
+      
     </div>
   );
 }
